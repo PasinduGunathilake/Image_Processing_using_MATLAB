087818478ab223e834f5f8d314f9a858ac00ae57
PasinduGunathilake <pasindugunathilake144@gmail.com>
2026-03-17 13:49:35 +0530

    fundamentals flie

---
+++ Fundamentals.m
@@ -0,0 +1,89 @@
+disp('Hello world');
+Hello world
+x=5;
+disp(x);
+     5
+
+x =x+5;
+disp(x);
+    10
+
+x = x-3;
+disp(x);
+     7
+
+x = x*4;
+x
+
+x =
+
+    28
+
+x =x/5;
+x
+
+x =
+
+    5.6000
+
+marks = 58;
+if (marks<40)
+disp('Fail');
+else
+disp('Pass');
+end
+Pass
+for i = 1:10
+disp(i);
+end
+     1
+
+     2
+
+     3
+
+     4
+
+     5
+
+     6
+
+     7
+
+     8
+
+     9
+
+    10
+
+for i = 2:2:10
+disp(i);
+end
+     2
+
+     4
+
+     6
+
+     8
+
+    10
+
+clear x
+x = 5;
+y = 10;
+z = x+y;
+z
+
+z =
+
+    15
+
+clear x y
+x
+Unrecognized function or variable 'x'.
+ 
+clear all
+y
+Unrecognized function or variable 'y'.
+ 
